fa248a72dcdb62cb8384e60cd9a356c6594a74b3
Harsini RS <133403437+harsinirs@users.noreply.github.com>
2024-05-18 17:20:34 +0530

    added script and data files

---
+++ using_BLAST.py
@@ -0,0 +1,38 @@
+#!usr/bin/python
+#Running BLAST over the Internet
+from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
+import sys
+import getopt
+
+
+filename=sys.argv[1]
+
+try:
+    f=open(filename)
+except IOError:
+    print("file %s does not exist" % filename)
+    
+print("running BLAST over internet")
+
+fasta_string = open(f).read()
+result_handle = NCBIWWW.qblast("blastn","nt", fasta_string)
+
+blast_record = NCBIXML.read(result_handle)
+len(blast_record.alignments) #get all alignments
+
+print("processing results")
+
+E_value_thresh=0.01
+for alignment in blast_record.alignments:
+    for hsp in alignment.hsps:
+        if hsp.expect <E_value_thresh:
+            print('****Alignment****')
+            print('sequence:', alignment.title)
+            print('length:', alignment.length)
+            print('e-value:', hsp.expect)
+            print(hsp.query)#query itself
+            print(hsp.match)#how it matches
+            print(hsp.sbjct)#print target that matches
+            
+print("End of page results.")
